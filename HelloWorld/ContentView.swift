c74cd17741dcff42e404fe02bb669dfb00fb38c3
Connor Nichols <cnichols9@huskers.unl.edu>
2022-01-27 12:35:31 -0600

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Connor Nichols on 1/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Connor Nichols on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
